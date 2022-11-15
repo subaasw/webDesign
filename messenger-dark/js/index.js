0be3d3d9479dbab255646de5f05100086c519f49
Subash Giri <subash.giri@zegal.com>
2022-11-15 14:13:39 +0545

    build: design added

---
+++ messenger-dark/js/index.js
@@ -0,0 +1,48 @@
+
+
+const line_items = [
+    {
+        "id": 1,
+        "name": "Will",
+        "description": "",
+        "product_id": "634e82a2c2741546fd61ed75",
+        "plan_code": "",
+        "price": 0,
+        "currency": "hkd",
+        "juris": "all"
+    },
+    {
+        "id": 1,
+        "name": "Will",
+        "description": "default plan",
+        "product_id": "",
+        "plan_code": "DL_DEFAULT_PLAN_10_2022",
+        "price": 99,
+        "currency": "hkd",
+        "juris": "hkg"
+    },
+    {
+        "id": 2,
+        "name": "LAWYER AS WITNESS",
+        "description": "Get a practicing lawyer as a witness",
+        "product_id": "",
+        "plan_code": "LAWYER_AS_WITNESS_ADDON_PLAN-10-2022",
+        "price": 100,
+        "currency": "hkd",
+        "juris": "hkg"
+    },
+    {
+        "id": 3,
+        "name": "LAWYER REVIEW",
+        "description": "Get your documents reviewed by a lawyer for expert advice. ",
+        "product_id": "",
+        "plan_code": "MURRAY_LAWYER_REVIEW_ADDON_PLAN-10-2022",
+        "price": 200,
+        "currency": "hkd",
+        "juris": "hkg"
+    }
+]
+
+const newLine = line_items.filter((item)=>)
+
+console.log(newLine);
